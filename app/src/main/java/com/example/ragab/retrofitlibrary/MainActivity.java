fc138675308374200773970608fc906012917532
AhmedAtef <ahmed.atef@marvelwall.com>
2018-12-22 16:08:41 +0200

    RetrofitLibrary

---
+++ app/src/main/java/com/example/ragab/retrofitlibrary/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ragab.retrofitlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ library_retrofit/src/main/java/com/example/library_retrofit/api/ApiClient.java
@@ -0,0 +1,21 @@
+package com.example.library_retrofit.api;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class ApiClient {
+
+    private static String BASE_URL = "https://";
+
+    private static Retrofit retrofit = null;
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
